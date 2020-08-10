6f2748284be81f70571b62aa2de9a4182a401236
dogra1598 <dogravishal1598@gmail.com>
2020-08-10 23:46:10 +0530

    signup and login routes completed with validation

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,37 +1,61 @@
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 const HttpError = require("../models/httpError");
 
 exports.postSignup = (req, res, next) => {
-  const username = req.body.username.value;
-  const email = req.body.email.value;
-  const password = req.body.password.value;
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
 
-  User.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        return next(new HttpError("User already exists", 422));
-      }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 422));
+  }
 
-      bcrypt
-        .hash(password, 12)
-        .then((hashedPassword) => {
-          const user = new User({
-            userName: username,
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] },
-          });
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        userName: username,
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] },
+      });
 
-          user.save();
-          return res.status(201).json({ message: "User added" });
-        })
-        .catch(() => {
-          throw new HttpError("Something went wrong", 500);
-        });
+      user.save();
+      return res.status(201).json({ message: "User added successfully." });
     })
-    .catch((err) => {
+    .catch(() => {
       throw new HttpError("Something went wrong", 500);
     });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError(errors.array()[0].msg, 422));
+  }
+
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return next(new HttpError("User with this email does not exists", 422));
+    }
+
+    bcrypt
+      .compare(password, user.password)
+      .then((result) => {
+        if (result) {
+          return res.status(200).json({ login: true });
+        }
+        return next(new HttpError("Password does not match", 401));
+      })
+      .catch(() => {
+        throw new HttpError("Something went wrong", 500);
+      });
+  });
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,47 @@
 const express = require("express");
+const { body } = require("express-validator");
+
+const User = require("../models/user");
 
 const router = express.Router();
 
 const authControllers = require("../controllers/authControllers");
 
-router.post("/signup", authControllers.postSignup);
+router.post(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("Please enter a valid Email.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userdoc) => {
+          if (userdoc) {
+            return Promise.reject(
+              "User with this email already exists. Please pick a different one."
+            );
+          }
+        });
+      }),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value != req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+      return true;
+    }),
+  ],
+  authControllers.postSignup
+);
+
+router.post(
+  "/login",
+  [
+    body("email")
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("Please enter a valid Email."),
+  ],
+  authControllers.postLogin
+);
 
 module.exports = router;
